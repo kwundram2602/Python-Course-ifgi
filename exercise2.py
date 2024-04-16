09108e25dde33dc423cf294e9ed2e85e85eeea9b
JanB <133894095+job2002@users.noreply.github.com>
2024-04-16 09:19:23 +0200

    add comments

--- exercise2.py
+++ exercise2.py
@@ -1,16 +1,22 @@
 def donuts(count):
+    # Declaring the output string
     text = "Number of Donuts: {}"
+    # Check if count is of type integer
     if isinstance(count, int):
+        # Check for the length and returning the strings accordingly to the assignment 
         if count < 10:
             return text.format(count)
         else: 
-            return text.format("many")         
+            return text.format("many")       
+    # If the input is not of type integer return a hint  
     else:
         return "The input has to be an integer."
         
 
 def verbing(s):
+    # Check if the input parameter s is of type string
     if isinstance(s, str):
+        # Check for the length and the endings, then change the ending accordingly
         if len(s) >= 3:
             if s.endswith("ing"):
                 s = s + "ly"
@@ -20,9 +26,9 @@ def verbing(s):
                 return s
         else: 
             return s
+    # If the input is not of type string return a hint
     else:
         return "The input has to be a string."
-    
 
 def main():
     print('donuts')
