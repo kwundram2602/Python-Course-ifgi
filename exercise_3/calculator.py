89d4e0ad37229c69ef4d330392c75210ebdceb5d
JanB <133894095+job2002@users.noreply.github.com>
2024-04-23 09:14:54 +0200

    test cases for calculator

--- exercise_3/calculator.py
+++ exercise_3/calculator.py
@@ -1,16 +1,16 @@
-class calculator:
+class Calculator:
     def add(self, number1 : int, number2 : int):
         return number1 + number2
     
-    def subtraction(self, number1 : int, number2 : int):
+    def subtract(self, number1 : int, number2 : int):
         return number1 - number2
     
     def multiply(self, number1 : int, number2 : int):
         return number1 * number2
     
-    def division(self, number1 : int, number2 : int):
+    def divide(self, number1 : int, number2 : int):
         if number2 != 0:
             return number1/number2
         else:
-            raise Exception("Man kann nicht durch 0 dividieren")
+            raise Exception("You cannot devide by zero")
     
--- exercise_3/main.py
+++ exercise_3/main.py
@@ -1,4 +1,9 @@
-from calculator import calculator
-calculator = calculator()
-print(calculator.division(1,5))
+from calculator import Calculator
+
+calculator = Calculator()
+print(calculator.add(7,5))
+print(calculator.subtract(34,21))
+print(calculator.multiply(54,2))
+print(calculator.divide(144,2))
+print(calculator.divide(45,0))
 
