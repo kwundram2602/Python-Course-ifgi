de96192cbf58edecdffca9cb91967c9157bea7eb
kwundram2602 <134778951+kwundram2602@users.noreply.github.com>
2024-05-06 16:31:48 +0200

    new solution

--- exercise_4/exercise_4_4.py
+++ exercise_4/exercise_4_4.py
@@ -1,23 +1,30 @@
-# change folder paths
-districts = r'D:/QGISworkspace/PIQUAG/Muenster/Muenster_City_Districts.shp'
-points = r'D:/QGISworkspace/PIQUAG/Muenster/Schools.shp'
-# change name if file already exists or delete file
-output = r'D:/QGISworkspace/PIQUAG/countedSchools7.gpkg'
-processing.run("native:countpointsinpolygon",
+import processing
+mc = iface.mapCanvas()
+districts = QgsProject.instance().mapLayersByName('Muenster_City_Districts')
+districts = districts[0]
+
+points = QgsProject.instance().mapLayersByName('Schools')
+points=points[0]
+
+pointsTool = processing.run("qgis:countpointsinpolygon",
 {'POLYGONS':districts,
 'POINTS':points,
 'WEIGHT':'','CLASSFIELD':'',
-'FIELD':'CountedSchools','OUTPUT':output})
-vlayer = QgsVectorLayer(output, "CountedSchools", "ogr")
-mc = iface.mapCanvas()
-if not vlayer.isValid():
-    print("Layer failed to load!")
-else:
-    QgsProject.instance().addMapLayer(vlayer)
-    
+'FIELD':None,'OUTPUT':'TEMPORARY_OUTPUT'})
 
-features =vlayer.getFeatures()
-print(vlayer.fields())
-for feature in features:
+countLayer = pointsTool['OUTPUT']
+dict = {}
+
+
+for feature in countLayer.getFeatures():
+    district= feature['P_DISTRICT']
+    numberSchools = feature['NUMPOINTS']
     
-    print(feature['P_District'], feature['CountedSchools'])
+    if district in dict:
+        dict[district] += numberSchools
+    else:
+        dict[district] = numberSchools
+
+for district, schools in dict.items():
+    print(district , schools)
+        
