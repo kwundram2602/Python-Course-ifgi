6609ff877b9d080fa88d1f73ab05c3279aec4dc3
kwundram2602 <134778951+kwundram2602@users.noreply.github.com>
2024-06-04 14:56:32 +0200

    infos parsed to pdf

--- exercise_7/exercise_7_1.py
+++ exercise_7/exercise_7_1.py
@@ -24,6 +24,8 @@ import time
 #import qgis modules
 from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (QgsProcessing,QgsProject,
+                        QgsVectorLayer,
+                        
                        QgsProcessingParameterFileDestination,
                        QgsFeatureSink,
                        QgsProcessingException,
@@ -49,17 +51,25 @@ class CreateCityDistrictProfile(QgsProcessingAlgorithm):
     # Constants used to refer to parameters and outputs. They will be
     # used when calling the algorithm from another algorithm, or when
     # calling from the QGIS console.
-
+    
     DISTRICTNAME = 'DISTRICTNAME'
     POINTINPUT = 'POINTINPUT'
     PDFOUTPUT = 'PDF_OUTPUT'
     
     # create pdf function
-    def createPDF(self, outputPath, attributeListe):
+    def createPDF(self, outputPath, attributeDict):
         c = canvas.Canvas(outputPath,pagesize = letter)
-        c.setFont("Helvetica", 12)
-        c.drawString(100,750,"Test")
-        
+        c.setFont("Helvetica", 20)
+        c.drawString(150,760,f"Information for the district '{attributeDict['district_name']}'")
+        c.setFont("Helvetica", 14)
+        c.drawString(100,735,f"The parent district is : {attributeDict['parent']}")
+        c.drawString(100,705,f"The district has an area of {attributeDict['area']} m^2")
+        c.drawString(100,690,f"The district has  {attributeDict['house_number_count']} housenumbers")
+        c.drawString(100,675,f"The district contains {attributeDict['parcels_count']} parcels")
+        if (attributeDict['counted_property']!="None"):
+            c.drawString(100,650,f"The counted property is '{attributeDict['counted_property']}'. There are {attributeDict['pool_or_school_count']} objects of this property")
+        else:
+            c.drawString(100,650,f"Please select pools or schools as point input")
         #if picturePath:
            # c.drawImage(picturePath,50, 50, width = 100 , height = 100)
         c.save()
@@ -126,7 +136,23 @@ class CreateCityDistrictProfile(QgsProcessingAlgorithm):
         parameters and outputs associated with it..
         """
         return self.tr("This processing script creates a PDF profile for a specific city district.")
+    # function definition to get alphabetical list
+    
+    def alphabeticalDistrictList(self):
+        # district layer
+        districts = QgsProject.instance().mapLayersByName('Muenster_City_Districts')[0]
+        # request order by nameclause
+        request = QgsFeatureRequest()
+        nameClause = QgsFeatureRequest.OrderByClause("Name",ascending = True)
+        orderby = QgsFeatureRequest.OrderBy([nameClause])
+        request.setOrderBy(orderby)
 
+        # name list 
+        district_names =[]
+        for feature in districts.getFeatures(request):
+            name= feature['NAME']
+            district_names.append(name)
+        return district_names
     def initAlgorithm(self, config=None):
         """
         Here we define the inputs and output of the algorithm, along
@@ -134,28 +160,11 @@ class CreateCityDistrictProfile(QgsProcessingAlgorithm):
         """
         
         
-        # function definition to get alphabetical list
-        def alphabeticalDistrictList():
-        # district layer
-            districts = QgsProject.instance().mapLayersByName('Muenster_City_Districts')[0]
-            # request order by nameclause
-            request = QgsFeatureRequest()
-            nameClause = QgsFeatureRequest.OrderByClause("Name",
-            ascending = True)
-            orderby = QgsFeatureRequest.OrderBy([nameClause])
-            request.setOrderBy(orderby)
-
-            # name list 
-            district_names =[]
-            for feature in districts.getFeatures(request):
-                name= feature['NAME']
-                district_names.append(name)
-            
-            return district_names
+        
         
         
         # create list of district names. Use user defined function
-        param_name_list= alphabeticalDistrictList()
+        param_name_list= self.alphabeticalDistrictList()
         
         # district name parameter
         self.addParameter(
@@ -169,8 +178,9 @@ class CreateCityDistrictProfile(QgsProcessingAlgorithm):
         self.addParameter(
             QgsProcessingParameterFeatureSource(
                 self.POINTINPUT,
-                self.tr('Input point layer'),
+                self.tr('Input point layer. Select Pools or Schools'),
                 [QgsProcessing.TypeVectorPoint]
+                
             )
         )
 
@@ -182,21 +192,92 @@ class CreateCityDistrictProfile(QgsProcessingAlgorithm):
                 fileFilter='PDF files (*.pdf)'
             )
         )
+    def count_of_layer(self,layer_name,dist_geom):
+        layer = QgsProject.instance().mapLayersByName(layer_name)[0]
+        layer.removeSelection()
+        parent = iface.mainWindow()
+        # get count of layer in district
+        for feature in layer.getFeatures():
+            if(feature.geometry().within(dist_geom)):
+                layer.selectByIds([feature.id()],QgsVectorLayer.AddToSelection)
+            
+        if(layer.selectedFeatureCount()>0):
+        #selected features
+            selected_features= layer.selectedFeatures()
+        
+        else:
+        # no features in district
+            print(f"no features of {layer_name} in this district")
+            selected_features=None
+            feature_count=0
+        
+        if(selected_features!=None):
+            feature_count=layer.selectedFeatureCount()
+        return feature_count
 
     def processAlgorithm(self, parameters, context, feedback):
         """
         Here is where the processing itself takes place.
         """
+        parent = iface.mainWindow()
+        # create list of district names. Use user defined function
+        param_name_list= self.alphabeticalDistrictList()
         
         #Create picturePath 
         #picturePath = self.districtImage()
-         
+        # load districts layer
+        districts = QgsProject.instance().mapLayersByName('Muenster_City_Districts')[0]
+        #get name of chosen district
+        district_name_index = self.parameterAsInt(parameters,'DISTRICTNAME',context)
+        district_name_title= param_name_list[district_name_index]
+        # get paretnt district
+        districts.selectByExpression("\"Name\" = '{}'".format(district_name_title),
+        districts.SetSelection)
+        selected_dist= districts.selectedFeatures()[0]
+        parent =selected_dist["P_District"]
+        
+        # get geom
+        dist_geom =selected_dist.geometry()
+        #get area
+        dist_area = dist_geom.area()
+        
+        # household layer count
+        house_number_count = self.count_of_layer('House_Numbers',dist_geom)
+        
+        # parcels count
+        parcels = QgsProject.instance().mapLayersByName('Muenster_Parcels')[0]
+        parcels.removeSelection()
+        parcels.selectByExpression("\"Name\" = '{}'".format(district_name_title),
+        parcels.SetSelection)
+        parcels_count=parcels.selectedFeatureCount()
+        
+        # pools or schools count
+        point_input = self.parameterAsCompatibleSourceLayerPath(parameters,
+        'POINTINPUT',context,compatibleFormats =['shp'])
+
+        if (point_input.endswith("public_swimming_pools.shp")):
+            pool_or_school_count= self.count_of_layer('public_swimming_pools',dist_geom)
+            counted_property="Pools"
+        elif(point_input.endswith("Schools.shp")):
+            pool_or_school_count= self.count_of_layer('public_swimming_pools',dist_geom)
+            counted_property="Schools"
+        else:
+            pool_or_school_count= 0
+            counted_property="None"
+            
+        #pdf output
         pdf_output = self.parameterAsFileOutput(parameters,'PDF_OUTPUT',context)
         print(f"Creating PDF for district: {parameters} at {pdf_output}")
         
+        # information for pdf create
+        dist_infos_dict = {'district_name':district_name_title,'parent': parent,
+        'area':dist_area,'house_number_count':house_number_count,'parcels_count':parcels_count,
+        'counted_property':counted_property,'pool_or_school_count':pool_or_school_count}
         
-        self.createPDF(pdf_output,"testlayer")
+        #create pdf
+        self.createPDF(pdf_output,dist_infos_dict)
         
-       
-        
-        return {'PDF_OUTPUT': pdf_output}
+        # return for debug
+        return {'PDF_OUTPUT': pdf_output, 'Point':point_input,'district_name':district_name_title,'parent': parent,
+        'area':dist_area,'house_number_count':house_number_count,'parcels_count':parcels_count,
+        'counted_property':counted_property,'pool_or_school_count':pool_or_school_count}
