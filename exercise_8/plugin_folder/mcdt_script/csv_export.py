6a857613ca4f350a411f8a97f42da06642f13f70
kwundram2602 <134778951+kwundram2602@users.noreply.github.com>
2024-06-11 14:09:06 +0200

    csv export

--- exercise_8/plugin_folder/mcdt_script/csv_export.py
+++ exercise_8/plugin_folder/mcdt_script/csv_export.py
@@ -1,1 +1,24 @@
 import csv
+def export_feature_csv(layer,outputpath):
+    #header for csv
+    attributes_header = layer.fields().names()
+    # get selected features
+    selected_features = layer.selectedFeatures()
+    
+    features_csv = []
+    # get feature data
+    for feature in selected_features:
+        feature_entry = feature.attributes()
+        features_csv.append(feature_entry)
+    csv_output= open(outputpath, 'w',newline='')
+    writer = csv.writer(csv_output,delimiter=';')
+    # write attributes header
+    writer.writerow(attributes_header)
+    # write features nto csv
+    for entry in features_csv:
+        writer.writerow(entry)
+    
+
+
+        
+    
